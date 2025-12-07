1333918515725c1dcd6de76b04a818f292dfbac2
Kuba <ksszewczykkuba@gmail.com>
2025-12-07 18:54:15 +0100

    change to db read

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.scheamas import PostCreate, PostReturn
 from app.db import Post, create_db_and_tables, get_async_session 
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,27 +12,37 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_post = {1: {"title": "New Post", "content": "Cool New Post"},
-             2: {"title": "Recenzja Filmu 'Gwiezdny Pył'", "content": "Wzruszająca opowieść fantasy z zaskakującymi zwrotami akcji"}, 
-             3: {"title": "Przepisy na wege obiady", "content": "Dziś proponujemy szybkie curry z ciecierzycą i batatami."}, 
-             4: {"title": "Raport Kwartalny Q3 2025", "content": "Wzrost przychodów o 15% w porównaniu do poprzedniego kwartału."}, 
-             5: {"title": "Ogłoszenie: Spotkanie Zespołu", "content": "Spotkanie dotyczące harmonogramu projektu odbędzie się w poniedziałek o 10:00."}, 
-             6: {"title": "Wskazówki Dotyczące SEO", "content": "Pamiętaj o optymalizacji meta tagów i budowaniu naturalnych linków zwrotnych."}}
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_async_session)):
+    post = Post(
+        caption = caption,
+        url = "dummy url",
+        file_type = "photo",
+        file_name = "dummy  name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
-@app.get("/posts")
-def get_all_posts(limit: int = None):
-    if limit:
-        return list(text_post.values())[:limit]
-    return text_post
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()] 
 
-@app.get("/posts/{id}")
-def get_post_id(id: int) -> PostReturn:
-    if id not in text_post:
-        raise HTTPException(status_code=404,detail="ID not found")
-    return text_post.get(id)
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
 
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostReturn: 
-    new_post = {"title": post.title, "content": post.content}
-    text_post[max(text_post.keys()) + 1 ] = new_post
-    return new_post
+        return {"post": posts_data}
